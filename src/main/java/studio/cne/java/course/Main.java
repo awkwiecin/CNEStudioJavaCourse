b0dfb194c8d4ecd7acd864b10a3e04ae2ed5744b
AWkwiecin <awkwiecin@gmail.com>
2019-05-17 14:30:24 +0200

    NewBranches

--- src/main/java/studio/cne/java/course/Main.java
+++ src/main/java/studio/cne/java/course/Main.java
@@ -1,18 +1,15 @@
 package studio.cne.java.course;
 
 
-import studio.cne.java.course.firstLesson.FirstLessonTest;
+import studio.cne.java.course.lesson1.FirstLessonTest;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world!");
-
-        System.out.println("-- TESTING LESSON 1 CLASSES --");
+        //System.out.println("hello world!");
         FirstLessonTest firstLessonTest = new FirstLessonTest();
         firstLessonTest.test();
 
-        System.out.println("-- APPLICATION OUT --");
     }
 }
---
+++ src/main/java/studio/cne/java/course/lesson2/SecondLessonTest.java
@@ -0,0 +1,11 @@
+package studio.cne.java.course.lesson2;
+
+public class SecondLessonTest {
+
+    public void test() {
+
+        System.out.println("-- LESSON 2 TEST BEGIN --");
+
+        System.out.println("-- LESSON 2 TEST END --");
+    }
+}
