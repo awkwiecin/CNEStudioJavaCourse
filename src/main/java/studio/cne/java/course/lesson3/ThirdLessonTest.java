dca94bbd5ff6b70bf3309b06e5d93ce885239270
AWkwiecin <awkwiecin@gmail.com>
2019-05-22 18:23:22 +0200

    rebuild

--- src/main/java/studio/cne/java/course/lesson3/ThirdLessonTest.java
+++ src/main/java/studio/cne/java/course/lesson3/ThirdLessonTest.java
@@ -4,6 +4,7 @@ public class ThirdLessonTest {
 
     public void test() {
         System.out.println("-- LESSON 2 TEST BEGIN --");
+        System.out.println("Justyna Janczy");
         System.out.println("-- LESSON 2 TEST END --");
     }
 
