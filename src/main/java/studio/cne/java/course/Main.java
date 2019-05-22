ffbac29542712ea5f255c5dfaec24d1f7491455c
AWkwiecin <awkwiecin@gmail.com>
2019-05-22 20:13:19 +0200

    new lesson template addition

--- src/main/java/studio/cne/java/course/Main.java
+++ src/main/java/studio/cne/java/course/Main.java
@@ -1,16 +1,17 @@
 package studio.cne.java.course;
 
-
 import studio.cne.java.course.lesson1.FirstLessonTest;
 import studio.cne.java.course.lesson2.SecondLessonTest;
 import studio.cne.java.course.lesson3.ThirdLessonTest;
+import studio.cne.java.course.lesson4.FourthLessonTest;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // FirstLessonTest.test();
-        // SecondLessonTest.test();
+//        FirstLessonTest.test();
+//        SecondLessonTest.test();
         ThirdLessonTest.test();
+//        FourthLessonTest.test();
     }
 }
---
+++ src/main/java/studio/cne/java/course/lesson4/FourthLessonTest.java
@@ -0,0 +1,10 @@
+package studio.cne.java.course.lesson4;
+
+public class FourthLessonTest {
+
+    public static void test() {
+        System.out.printf("-- LESSON 4 TEST BEGIN --");
+        System.out.printf("-- LESSON 4 TEST END --");
+    }
+
+}
