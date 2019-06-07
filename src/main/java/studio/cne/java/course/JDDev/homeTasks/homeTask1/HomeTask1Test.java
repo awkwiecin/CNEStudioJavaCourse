735a1ee0734cc0624a020e83c18e5e0b6520dcb7
AWkwiecin <awkwiecin@gmail.com>
2019-06-07 17:09:16 +0200

    commit for addendum of packages

---
+++ src/main/java/studio/cne/java/course/JDDev/homeTasks/homeTask1/HomeTask1Test.java
@@ -0,0 +1,11 @@
+package studio.cne.java.course.JDDev.homeTasks.homeTask1;
+
+public class HomeTask1Test {
+
+    public static void test() {
+
+        System.out.println("--HomeTask 1 TEST BEGIN --");
+        System.out.println("--HomeTask 1 TEST END --");
+
+    }
+}
---
+++ src/main/java/studio/cne/java/course/JDDev/homeTasks/homeTask2/HomeTask2Test.java
@@ -0,0 +1,11 @@
+package studio.cne.java.course.JDDev.homeTasks.homeTask2;
+
+public class HomeTask2Test {
+
+    public static void test() {
+
+        System.out.println("--HomeTask 2 TEST BEGIN --");
+        System.out.println("--HomeTask 2 TEST END --");
+
+    }
+}
