4311037148253673ded33be8915ae237cc1ad66c
AWkwiecin <awkwiecin@gmail.com>
2019-06-07 15:30:06 +0200

    New branch

---
+++ src/main/java/studio/cne/java/course/JDDev/lesson1/Lesson1Test.java
@@ -0,0 +1,12 @@
+package studio.cne.java.course.JDDev.lesson1;
+
+public class Lesson1Test {
+
+    public static void test() {
+
+        System.out.println("-- JDDev Lesson 1 TEST BEGIN --");
+        System.out.println("-- JDDev Lesson 1 HomeTask 1 TEST END --");
+
+    }
+
+}
--- src/main/java/studio/cne/java/course/Main.java
+++ src/main/java/studio/cne/java/course/Main.java
@@ -5,11 +5,8 @@ import studio.cne.java.course.JJDev.lesson4.FourthLessonTest;
 public class Main {
 
     public static void main(String[] args) {
-//        FirstLessonTest.test();
-//         SecondLessonTest.test();
-//        ThirdLessonTest.test();
-        FourthLessonTest.test();
-//        Hometask2Test.test();
 
-    }
+
+
+     }
 }
