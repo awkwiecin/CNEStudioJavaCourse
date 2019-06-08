c6d3af1afaad5ff47dd1a89183757e160b324595
JJDEV <justyna5252@gmail.com>
2019-06-08 16:46:17 +0200

    TAXCalculator

---
+++ src/main/java/studio/cne/java/course/JJDev/homeTasks/homeTask1/Student.java
@@ -0,0 +1,40 @@
+package studio.cne.java.course.JJDev.homeTasks.homeTask1;
+
+public class Student {
+    private int physicsGrade;
+    private int mathGrade;
+    private int englishGrade;
+    private int polishGrade;
+    private int peGrade;
+
+    public int getPhysicsGrade() {
+        return physicsGrade;
+    }
+
+    public int getMathgrade() {
+        return mathGrade;
+    }
+
+    public int getEnglishGrade() {
+        return englishGrade;
+    }
+
+    public int getPolishGrade() {
+        return polishGrade;
+    }
+
+    public int getPeGrade() {
+        return peGrade;
+    }
+
+    public Student(int physicsGrade, int mathgrade, int englishGrade, int polishGrade, int peGrade) {
+        this.physicsGrade = physicsGrade;
+        this.mathGrade = mathgrade;
+        this.englishGrade = englishGrade;
+        this.polishGrade = polishGrade;
+        this.peGrade = peGrade;
+
+
+    }
+
+}
