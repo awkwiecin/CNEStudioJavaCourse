f61b6dd309aa183caef71b1b9710dddfb7d45b7b
JJDEV <justyna5252@gmail.com>
2019-06-17 18:22:13 +0200

    library stock

--- src/main/java/studio/cne/java/course/JJDev/homeTasks/homeTask2/Hometask2Test.java
+++ src/main/java/studio/cne/java/course/JJDev/homeTasks/homeTask2/Hometask2Test.java
@@ -3,19 +3,22 @@ package studio.cne.java.course.JJDev.homeTasks.homeTask2;
 //import java.util.ArrayList;
 //import java.util.List;
 
+import studio.cne.java.course.JJDev.commonConstantsAndMethods.CommonMethods;
+
 public class Hometask2Test {
 
     public static void test() {
 
         System.out.println("--HomeTask 2 TEST BEGIN --");
 
-        Book book0 = new Book ("Zbrodnia i kara", "F.Dostojewski",2018, "Czesław Jastrzębiec-Kozłowski", false);
-        Book book1 = new Book ("Tajemna historia", "D.Tartt",2015, "Jerzy Kozłowski", false );
-        Book book2 = new Book("Kafka nad morzem", "H.Murakami", 2007,"Anna Zielińska-Elliott", true);
-        Book book3 = new Book("Małe życie", "Hanya Yanagihara",2016,"Jolanta Kozak", false);
-        Book book4= new Book ("Bezdomny ptak","Gloria Whelan",2008,"unknown",true);
+        Library library = new Library("Słoneczne kartki");
+        CommonMethods.createLibraryData(library);
 
-  //      List<Book> books = new ArrayList<Book>();
+       // library.printLibraryStock();
+
+        library.rent("Małe życie");
+        library.receive("Tajemna historia");
+        library.receive("Małe życie");
 
 
 
