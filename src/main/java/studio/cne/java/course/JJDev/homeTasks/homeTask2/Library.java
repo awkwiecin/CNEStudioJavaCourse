959a38c8b1add753cad5de05f1e089d1c6fe2dd5
JJDEV <justyna5252@gmail.com>
2019-06-17 02:35:14 +0200

    HT2

---
+++ src/main/java/studio/cne/java/course/JJDev/homeTasks/homeTask2/Library.java
@@ -0,0 +1,15 @@
+package studio.cne.java.course.JJDev.homeTasks.homeTask2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+     List <Book> books = new ArrayList<Book>();
+     public boolean rentBook (boolean rented){
+         true = books.remove (book);
+
+     }
+
+
+}
