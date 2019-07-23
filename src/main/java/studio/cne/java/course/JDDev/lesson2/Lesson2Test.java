3997793933a7531ded76b57538ba354fff24cbd6
AWkwiecin <awkwiecin@gmail.com>
2019-07-23 15:12:58 +0200

    standardization of the project. Official version 1.0.

--- src/main/java/studio/cne/java/course/JDDev/lesson2/Lesson2Test.java
+++ src/main/java/studio/cne/java/course/JDDev/lesson2/Lesson2Test.java
@@ -5,7 +5,7 @@ public class Lesson2Test {
     public static void test() {
 
         System.out.println("-- JDDev Lesson 2 TEST BEGIN --");
-        System.out.printf("Magia!");
+
         System.out.println("-- JDDev Lesson 2 HomeTask 1 TEST END --");
 
     }
